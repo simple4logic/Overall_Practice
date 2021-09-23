559c28db41f877a9cf00868eaf06c845e73f068e
Hyuntae H <okht12@gmail.com>
2021-09-23 14:20:19 +0000

    .

---
+++ Practice/iotest.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i = 1;
+
+    if(i = 0){
+        cout << "if 통과!" << endl;
+        return 0;
+    }
+
+    cout << "if 통과 실패" << endl;
+}
