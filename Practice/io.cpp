62aefcd39d1035b6413ec480fc1af7bc647e9522
Hyuntae H <okht12@gmail.com>
2021-09-24 14:24:13 +0000

    .

--- Practice/io.cpp
+++ Practice/io.cpp
@@ -2,18 +2,12 @@
 using namespace std;
 
 int main(){
-    int i;
-    int j;
-    char str[101];
-    cin >> str;
-
-    for(j=0;j<10;j++){
-        for(i=0;i<10;i++){
-            if(str[i+10*j]=='\0'){
-                return 0;
-            }
-            printf("%c", str[i+10*j]);
-        }
-        cout << endl;
+    char str[11];
+    int a;
+    while((a =scanf("%10s", &str))==1){
+        printf("scanf return : %d\n", a);      
+        cout << str << endl;
     }
+
+    printf("last scanf return value : %d\n", a);
 }
