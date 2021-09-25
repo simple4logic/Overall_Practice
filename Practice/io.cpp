58a2d5e8a264b53caa24149e0acaed753f63743a
Hyuntae H <okht12@gmail.com>
2021-09-25 13:56:24 +0000

    .

--- Practice/io.cpp
+++ Practice/io.cpp
@@ -1,12 +1,27 @@
-#include<stdio.h>
-int main()
-{
- int n,z,x;
- scanf("%d",&n);
- for(z=1;z<=n;z++){
-  for(x=0;x!=n-z;x++)printf("1");
-  for(x=0;x!=z;x++)printf("*1");
-  puts("");
- }
- return 0;
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int cnt = 0;
+    cin >> n;
+
+    while(n!=1){
+        if(n%3==0){
+            n/=3;
+            cnt++;
+            printf("used 3division\n");
+            continue;
+                    }
+        if(n%2==0){
+            n/=2;
+            cnt++;
+            printf("used 2division\n");
+            continue;
+        }
+        n--;
+        printf("used 1minus\n");
+        cnt++;
+    }
+    printf("%d\n", cnt);
 }
--- Practice/iotest.cpp
+++ Practice/iotest.cpp
@@ -1,17 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n, b;
-    cin >> n;
+int cbn(int n, int r)
+{
+    if(n == r || r == 0) 
+        return 1; 
+    else 
+        return cbn(n - 1, r - 1) + cbn(n - 1, r);
+}
 
-    for(b=0;b<n;b++){
-        for(int i=0;i<n-b-1;i++){
-            printf(" ");
-        } 
-        for(int j=0;j<b+1;j++){
-            printf("* ");
-        }
-        printf("\n");
+int main(){
+    int x;
+    int y=0;
+    int sum=0;
+    cin >> x;
+    while(x>y){
+        sum+=cbn(x, y);
+        x--; y++;
     }
+
+    printf("%d", sum%1007);
 }
