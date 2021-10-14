9e4483565bab079aaa6eee5ad6bf1d7a763ebb43
Hyuntae H <okht12@gmail.com>
2021-10-14 15:00:43 +0000

    .

--- C++/Practice_C++/arr.cpp
+++ C++/Practice_C++/arr.cpp
@@ -1,15 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    string str;
-    int ans=0, len=0;
-    while(cin.getline(str,'\n')){
-        if(str[len]=' ')
-            ans++;
-        len++;
-    }
+int f(char x);
 
-    if(str[0]==' ') ans--;
-    if(str[len]==' ') ans--;
+int main(){
+    char n;
+    int sum = 0;
+    while(~scanf(" %c", &n)){
+        sum+=f(n);
+    }
+    printf("%d", sum);    
+}
+
+int f(char x){
+    int y = 0;
+    if(x>86)
+        y++;
+    if(x>83)
+        y++;
+    if(x>79)
+        y++;
+    if(x>76)
+        y++;
+    if(x>73)
+        y++;
+    if(x>70)
+        y++;
+    if(x>67)
+        y++;
+    y+=3;
+    return y; 
 }
--- C++/Practice_C++/io.cpp
+++ C++/Practice_C++/io.cpp
@@ -2,19 +2,9 @@
 using namespace std;
 
 int main(){
-    int a, b;
+    int a, b, c;
     scanf("%d %d", &a, &b);
-    vector<int> A, B;
-    for(;a;){ //until A<1
-        A.push_back(a%10); //맨 끝자리 삽입
-        B.push_back(b%10);
-        a /= 10; // 맨끝자리 삭제
-        b /= 10;
-    }
-    int x = 0, y = 0;
-    for (auto a : A)
-        x = x*10 + a;
-    for (auto a : B)
-        y = y*10 + a;
-    printf("%d", max(x, y));
+    printf("your input : %d %d\n", a, b);
+    scanf("%c", &c);
+    printf("in ascii : %d\n", c);
 }
