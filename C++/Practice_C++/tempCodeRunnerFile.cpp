1ceb6260ddfa3f95cfcec7a4ec21f32a7d80446c
Hyuntae H <okht12@gmail.com>
2021-11-12 13:09:50 +0000

    .

--- C++/Practice_C++/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    double pi = 3.1415926536;
-    int n;
-    scanf("%d", &n);
-    printf("%.6lf\n%lf",(double)pi*n*n, (double)2*n*n);
-}
