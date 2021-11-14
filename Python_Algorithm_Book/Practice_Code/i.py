a0a0e92c19e270cbd1ce8b8cf28ddfca6184c362
Hyuntae H <okht12@gmail.com>
2021-11-14 14:55:32 +0000

    .

--- Python_Algorithm_Book/Practice_Code/i.py
+++ Python_Algorithm_Book/Practice_Code/i.py
@@ -1,41 +1,14 @@
-#Q1 Valid Palindrome
-
-'''
-pre-processing by filtering input.
-isalnum() is the method that checks whether object is (number or english) or not.
-'''
-
-'''
-#this is the pre-processing function that made input regular lower-case english(or number).
-arr = []
-for char in arr:
-    if char.isalnum(): #to check input 'char' is num&Eng or not, if true, return true. Vice versa.
-        arr.append(char.lower()) #if true, append it to the list.
-'''
-
-#1. my solution
+from typing import List
 
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        arr = []
+    def reverseString(self, s: List[str]) -> None:
+        for i in range(len(s)//2):
+            a: str = s[i] #save first
+            print(a)
+            s[i]=s[-(i+1)]
+            print(s[i])
+            s[-(i+1)] = a
+        print(s)
 
-        for char in s:
-            if char.isalnum():
-                arr.append(char.lower())
-
-        a: int = 1
-        cnt: int = 0
-
-        for element in arr:
-            if element == arr[-a]:
-                cnt+=1
-                a+=1
-
-        if cnt == len(arr):
-            return true
-        else:
-            return false
-        
 s = Solution()
-s.isPalindrome("A man, a plan, a canal: Panama")
-
+s.reverseString(["a","b","c","d"])
