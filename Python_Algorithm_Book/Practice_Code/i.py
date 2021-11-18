12da23cb31f6f0c648b6217e9676fb4263bdf188
Hyuntae H <okht12@gmail.com>
2021-11-18 14:12:05 +0000

    .

--- Python_Algorithm_Book/Practice_Code/i.py
+++ Python_Algorithm_Book/Practice_Code/i.py
@@ -3,16 +3,19 @@ import re
 from collections import Counter
 
 class Solution:
-    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        a = paragraph.lower()
-        a = re.sub('[\W]',' ',a) #특수문자들 모두 space로 변경
-        lis = a.split() # split by space
-        cntdic = Counter(lis)
-        t = banned[0]
-        if t in cntdic:
-            del cntdic[t]
-        #print(max(cntdic.keys()))
+    def stringsort(self, strs: str) -> str:
+        la = sorted(strs)
+        return "".join(la)   
 
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic={}
+        for words in strs:
+            dic[words] = stringsort(words) #word : sorted word
+        print(dic)
+
+
+
+#for testing
 a = Solution()
-s = "Bob hit a ball, the hit BALL flew far after it was hit."
-a.mostCommonWord(s, ["hit"])
+s = ["eat","tea","tan","ate","nat","bat"]
+a.groupAnagrams(s)
