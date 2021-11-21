e56f92e75893197bb7abe047b1a63d4f11a74b59
Hyuntae H <okht12@gmail.com>
2021-11-21 14:15:27 +0000

    .

--- Python_Algorithm_Book/Practice_Code/i.py
+++ Python_Algorithm_Book/Practice_Code/i.py
@@ -3,15 +3,5 @@ import re
 import collections
 
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dic=collections.defaultdict(list)
-        #dic={}
-
-        for words in strs:
-            dic[''.join(sorted(words))].append(words)
-        print(list(dic.values()))
-
-#for testing
-a = Solution()
-s = ["eat","tea","tan","ate","nat","bat"]
-a.groupAnagrams(s)
+    def longestPalindrome(self, s: str) -> str:
+        
