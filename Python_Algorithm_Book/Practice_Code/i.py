79b928913873bf108ccc014ff2093022c3fa2aaf
Hyuntae H <okht12@gmail.com>
2021-11-22 14:16:13 +0000

    .

--- Python_Algorithm_Book/Practice_Code/i.py
+++ Python_Algorithm_Book/Practice_Code/i.py
@@ -4,23 +4,27 @@ import collections
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        def expand(left: int, right: int) ->str:
+            while left >= 0 and right < len(s) and s[left]==s[right]:
+                left-=1
+                right+=1
+            #when escaped from the while loop, this array has much more index than palindrome.
+            #ZXXXXY condition or XXXXY (left is - index) or ZXXXX (right exceeds the length)
+            #therefore returns the index "left+1", and "right"(in string slicing, :right returns until right-1 index)
+            return s[left+1:right]
+
         if len(s) < 2 or s==s[::-1]: #filtering - single letter or whole palindrome
             return s
 
+        result = '' #gen blank string
+        for i in range(len(s)-1):
+            result = max(result,
+                            expand(i, i+1), #abbc : even number two pointer
+                            expand(i, i+2), #axyxc : odd number two pointer
+                            key=len #the criteria for checking max is length of string
+                            )
+        return result
 
-
-    def isPalindrome(self, s: str) -> bool:
-        arr = []
-        for char in s:
-            if char.isalnum():
-                arr.append(char.lower()) #loop for pre-processing
-        a: int = 1
-        cnt: int = 0
-        for element in arr: #swipe array to check [0] == [-1], next [1]==[-2], ···
-            if element == arr[-a]:
-                cnt+=1
-                a+=1
-        if cnt == len(arr):
-            return true
-        else:
-            return false
+a = Solution()
+s = "babad"
+print(a.longestPalindrome(s))
