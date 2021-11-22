15a65b026d66cf67dedb03fe9dbbf3c3adc65d10
Hyuntae H <okht12@gmail.com>
2021-11-22 14:30:35 +0000

    .

--- Python_Algorithm_Book/Practice_Code/i.py
+++ Python_Algorithm_Book/Practice_Code/i.py
@@ -3,28 +3,9 @@ import re
 import collections
 
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        def expand(left: int, right: int) ->str:
-            while left >= 0 and right < len(s) and s[left]==s[right]:
-                left-=1
-                right+=1
-            #when escaped from the while loop, this array has much more index than palindrome.
-            #ZXXXXY condition or XXXXY (left is - index) or ZXXXX (right exceeds the length)
-            #therefore returns the index "left+1", and "right"(in string slicing, :right returns until right-1 index)
-            return s[left+1:right]
-
-        if len(s) < 2 or s==s[::-1]: #filtering - single letter or whole palindrome
-            return s
-
-        result = '' #gen blank string
-        for i in range(len(s)-1):
-            result = max(result,
-                            expand(i, i+1), #abbc : even number two pointer
-                            expand(i, i+2), #axyxc : odd number two pointer
-                            key=len #the criteria for checking max is length of string
-                            )
-        return result
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
 
 a = Solution()
-s = "babad"
-print(a.longestPalindrome(s))
+nums = [2,7,11,15], target = 9
+print(a.twoSum(nums, target))
