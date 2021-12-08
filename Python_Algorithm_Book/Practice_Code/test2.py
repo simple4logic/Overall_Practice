2976a16967141234a87f3ed54228c8add6ffe751
Hyuntae H <okht12@gmail.com>
2021-12-08 14:48:29 +0000

    .

--- Python_Algorithm_Book/Practice_Code/test2.py
+++ Python_Algorithm_Book/Practice_Code/test2.py
@@ -7,11 +7,22 @@ import collections
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        tmp=[]
-        for i in range(len(nums)):
-            p = 1
-            
+        
+        tmp1=[1 for i in range(len(nums))]
+        tmp2=[1 for j in range(len(nums))]
 
+        for i in range(len(nums)):
+            if i == 0: #for i-1 index
+                continue
+            tmp1[i] = tmp1[i-1] * nums[i-1]
+            
+        for j in range(len(nums)):
+            if j == 0: #for i-1 index
+                continue
+            tmp2[j] = tmp2[j-1] * nums[-1-(j-1)]
+
+        tmp3 = list(map(lambda x,y : x*y,tmp1, reversed(tmp2)))
+        return tmp3
 
 a = Solution()
 nums = [1,2,3,4]
