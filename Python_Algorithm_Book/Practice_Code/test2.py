993d012cabb3f4ef4bb8d399d40d1b2843c0bab7
Hyuntae H <okht12@gmail.com>
2021-12-09 14:37:20 +0000

    .

--- Python_Algorithm_Book/Practice_Code/test2.py
+++ Python_Algorithm_Book/Practice_Code/test2.py
@@ -1,32 +1,22 @@
-#Q561 Array Partition I
-# 12/06 start p190 in the book
-
 from typing import List
 import re
 import collections
+import sys
 
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
-        tmp1=[1 for i in range(len(nums))]
-        tmp2=[1 for j in range(len(nums))]
+    def maxProfit(self, prices: List[int]) -> int:
+        max_price = 0 #max = -sys.maxsize
+        min_val = prices[0] #min sys.maxsize
 
-        for i in range(len(nums)):
-            if i == 0: #for i-1 index
-                continue
-            tmp1[i] = tmp1[i-1] * nums[i-1]
-            
-        for j in range(len(nums)):
-            if j == 0: #for i-1 index
-                continue
-            tmp2[j] = tmp2[j-1] * nums[-1-(j-1)]
+        for N in prices:
+            min_val = min(min_val, N)
+            max_price = max(max_price, N-min_val)
 
-        tmp3 = list(map(lambda x,y : x*y,tmp1, reversed(tmp2)))
-        return tmp3
+        return max_price
 
 a = Solution()
-nums = [1,2,3,4]
-print(a.productExceptSelf(nums))
+s = [4, 6, 5, 8, 10, 5, 9, 3, 5, 12, 1, 10, 11, 7]
+print(a.maxProfit(s))
 
 #Output: [24,12,8,6]
         
