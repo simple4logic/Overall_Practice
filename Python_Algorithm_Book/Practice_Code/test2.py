b8a5e00f4af7b23cf9d3a3b7983b7cc8b5cca874
Hyuntae H <okht12@gmail.com>
2021-12-10 22:58:12 +0000

    .

--- Python_Algorithm_Book/Practice_Code/test2.py
+++ Python_Algorithm_Book/Practice_Code/test2.py
@@ -1,22 +1,46 @@
 from typing import List
 import re
 import collections
-import sys
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        max_price = 0 #max = -sys.maxsize
-        min_val = prices[0] #min sys.maxsize
+    def isPalindrome(self, head: ListNode) -> bool:
+        rev = None #define address of reverse string as null
+        slow = fast = head #set start point
 
-        for N in prices:
-            min_val = min(min_val, N)
-            max_price = max(max_price, N-min_val)
+        while fast and fast.next: #when both are true
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+            #making reverse linked list until the middle point
+        if fast:
+            slow = slow.next #when odd length, move one more
 
-        return max_price
+        while rev and (rev.val==slow.val):
+            slow, rev = slow.next, rev.next #next node
+        return not rev
+        '''
+        do it until rev reaches the end or not palindrome 
+        when reaches the end, rev is none. It means palindrome so rev = 
+        '''
+'''
+rev <- slow
+rev.next <- rev
+slow <- slow.next
 
+'''
+
+#to test, you have to make to linked-list manually. 
+inp = [1,2,2,1] #inp = input as head
+node3 = ListNode(inp[3])
+node2 = ListNode(inp[2], node3)
+node1 = ListNode(inp[1], node2)
+head = ListNode(inp[0], node1)
+
+
+#for test
 a = Solution()
-s = [4, 6, 5, 8, 10, 5, 9, 3, 5, 12, 1, 10, 11, 7]
-print(a.maxProfit(s))
-
-#Output: [24,12,8,6]
-        
+a.isPalindrome(head)
