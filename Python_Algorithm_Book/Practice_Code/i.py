15595c21361e7cb136e7e4b3e3ff8e88386cf293
Hyuntae H <okht12@gmail.com>
2021-12-10 14:41:24 +0000

    dir changed

--- Python_Algorithm_Book/Practice_Code/i.py
+++ Python_Algorithm_Book/Practice_Code/i.py
@@ -7,5 +7,35 @@ class ListNode:
         self.val = val
         self.next = next
 
-node = ListNode([1,2,2,1])
-print(node)
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        if not head:
+            return True #to prevent the event that input is 0 node. "But" in this Q, node count can not be zero
+
+        ansl :list = []
+
+        node = head
+
+        while node is not None:
+            ansl.append(node.val)
+            node = node.next
+
+        while len(ansl)>1: #if len = 1, stop. Actually single letter left so true.// if len = 0, stop.
+            if ansl.pop(0) != ansl.pop():
+                return False
+        
+        return True
+
+#to test, you have to make to linked-list manually. 
+inp = [1,2,2,1] #inp = input as head
+node3 = ListNode(inp[3])
+node2 = ListNode(inp[2], node3)
+node1 = ListNode(inp[1], node2)
+head = ListNode(inp[0], node1)
+
+
+#for test
+a = Solution()
+a.isPalindrome(head)
+
+
