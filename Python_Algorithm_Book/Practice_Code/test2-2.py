fbd00785ce9f9ba6b31715dfae4243cf6c115f9a
Hyuntae H <okht12@gmail.com>
2021-12-18 14:56:56 +0000

    .

--- Python_Algorithm_Book/Practice_Code/test2-2.py
+++ Python_Algorithm_Book/Practice_Code/test2-2.py
@@ -8,58 +8,45 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        root = head = ListNode(0)
+    def swapPairs(self, head: ListNode) -> ListNode:
+        node = head
+        if not node or not node.next: #when (0 or 1 node)
+           return node
+        
+        head_node = ListNode(0, node.next) #point second node
+        prev = ListNode()
 
-        carry = 0
-        while l1 or l2 or carry: 
-            #각 자릿수가 모두 더해질때까지! 만약 자릿수가 같고 carry in이 존재하면 한번 더 루프 돌려서
-            #다음 자리까지 노드 하나 더 추가해줘야함. 즉 노드 개수 = 더 긴 리스트 길이 + 1
-
-            #sum 매번 초기화
-            sum = 0
-
-            #두 리스트의 "자릿수"를 더함, 존재하는 경우에만 더함. 자릿수가 다른 수를 더할 수도 있음.
-            if l1:
-                sum+=l1.val
-                l1 = l1.next
-            if l2:
-                sum+=l2.val
-                l2 = l2.next
-
-            #divmod = A//10, A%10, 즉 10으로 나눈 몫과 나머지를 리턴
-            carry, val = divmod(sum+carry, 10) #전 자리수에서 온 carry은 해봤자 1 or 0이다.
-            head.next = ListNode(val)
-            head = head.next
-
-        return root.next
+        while node and node.next: #노드가 하나면,첨부터 false이므로 루프 통과X
+            prev.next = node.next
+            temp = node
+            node = node.next
+            temp.next = node.next
+            node.next = temp
+            prev = temp
+            print(node.val, temp.val)
+            node = node.next.next
+        
+        return head_node.next
             
 
 
 #to test, you have to make to linked-list manually. 
-inp1 = [9,3]
-inp2 = [4,5]
-# inp1 = [1]
-# inp2 = [3]
+inp1= [1,2,3,4, 5,6,7,8,9]
+def makeLinkedList(inp: List) -> ListNode:
+    node = ListNode()
+    head = node
+    for val in inp:
+        new_node = ListNode(val)
+        new_node.next = node.next
+        node.next = new_node
+        node = node.next
+    return head
 
-#list1_3 = ListNode(inp1[2], None)
-list1_2 = ListNode(inp1[1], None)
-list1 = ListNode(inp1[0], list1_2)
-
-#list2_3 = ListNode(inp2[2], None)
-list2_2 = ListNode(inp2[1], None)
-list2 = ListNode(inp2[0], list2_2)
-
-# list2 = ListNode(inp2[0], None)
-# list1 = ListNode(inp1[0], None)
-
-# list1 = ListNode(0, None)
-# list2 = ListNode(0, None)
+link1 = makeLinkedList(inp1).next
 
 #for test
 a = Solution()
-#list1 = [1,2,4], list2 = [1,3,4]
-head_node = a.addTwoNumbers(list1, list2)
+head_node = a.swapPairs(link1)
 while head_node:
     print(head_node.val)
     head_node = head_node.next
