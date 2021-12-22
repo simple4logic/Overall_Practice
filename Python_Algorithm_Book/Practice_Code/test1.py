a4bd7de27e5974cf0fa1b06270b8890767096d33
Hyuntae H <okht12@gmail.com>
2021-12-22 14:49:01 +0000

    .

--- Python_Algorithm_Book/Practice_Code/test1.py
+++ Python_Algorithm_Book/Practice_Code/test1.py
@@ -2,17 +2,32 @@ from typing import List
 import re
 import collections
 
+# t is between 30 ~ 70
+# T.index(element) returns index
+
 class Solution:
-    def removeDuplicateLetters(self, s: str) -> str:
-        for char in sorted(set(s)):
-            suffix = s[s.index(char):]
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        T = temperatures
+        ans, stack = [0 for i in range(len(T))], [] #init ans with 0
+
+        for val in T:
+            cur_idx = T.index(val)
             
-            #전체 집합과 접미사 집합이 일치할 경우 분리한다
-            if set(s) == set(suffix):
-                return char + self.removeDuplicateLetters()
-        return ''
+            if not stack: #스택에 없다면 append / 첫번째 원소 filetering
+                stack.append(val)
+                continue
+            
+            while stack and stack[-1] < val: #stack이 있고, 마지막 값보다 지금 값이 더 크면 반복
+                last_idx = T.index(stack.pop())
+                stack.append(val)
+                ans[last_idx] = cur_idx - last_idx
+
+            print(stack, ans, cur_idx)
+
+        return ans  
+        
         
 
 a = Solution()
-s = "cbacdcbc"
-print(a.removeDuplicateLetters(s))
+t = [73,74,75,71,69,72,76,73]
+print(a.dailyTemperatures(t))
