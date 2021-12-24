152d7114b7ca8cc1273e9ce0e39636958af51342
Hyuntae H <okht12@gmail.com>
2021-12-24 14:31:13 +0000

    .

--- Python_Algorithm_Book/Practice_Code/i.py
+++ Python_Algorithm_Book/Practice_Code/i.py
@@ -6,30 +6,40 @@ class MyStack:
 
     def __init__(self):
         #def with the type deque
-        stack = collections.deque()
+        self.q = collections.deque()
 
     def push(self, x: int) -> None:
-        stack.append(x)
+        self.q.append(x)
 
     def pop(self) -> int:
-        return stack.pop()
+        self.q.reverse()
+        pop = self.q.popleft()
+        self.q.reverse()
+        return pop
 
     def top(self) -> int:
-        top = stack.pop()
-        stack.append(top)
+        self.q.reverse()
+        top = self.q.popleft()
+        self.q.reverse()
+        self.q.append(top)
         return top         
 
     def empty(self) -> bool:
-        if self:
-            return True
-        else:
+        if self.q:
             return False
+        else:
+            return True
         
 
 
 # Your MyStack object will be instantiated and called as such:
 obj = MyStack()
 obj.push(10)
+obj.push(9)
+obj.push(8)
 param_2 = obj.pop()
+print(obj.q)
 param_3 = obj.top()
+print(obj.q)
 param_4 = obj.empty()
+print(param_4, obj.q)
