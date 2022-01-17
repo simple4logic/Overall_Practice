fe2cd63b8f22e60e41d38f77bcbb20a9e3d28a2e
Hyuntae H <okht12@gmail.com>
2022-01-17 14:27:17 +0000

    .

--- Python_Algorithm_Book/Practice_Code/i.py
+++ Python_Algorithm_Book/Practice_Code/i.py
@@ -1,85 +1,29 @@
 from typing import List
 import re
 import collections
+import heapq
 
 class Node():
-    def __init__(self, val = None, prev = None, next = None): #값, 전 주소, 다음 주소
+    def __init__(self, val = 0, next = None):
         self.val = val
-        self.prev = prev
         self.next = next
 
-class MyCircularDeque:
+class Solution():
+    def mergeLists(self, lists: List[Node]) -> Node:
+        root = result = Node(None) #save result's address at root
+        heap = []
 
-    def __init__(self, k: int):
-        self.head, self.tail = Node(), Node()
-        self.head.next = self.tail
-        self.tail.prev = self.head
-        self.maxlen = k
-        self.cnt = 0
+        for i in range(len(lists)):
+            if lists[i]:
+                headq.headpush(heap, (lists[i].val), i, lists[i])
+        
+        while heap:
+            node = headq.headpop(heap)
+            idx = node[1]
+            result.next = node[2]
 
-    def _add(self, node: Node(), new_node: Node()):
-        a = node.next
-        node.next = new_node
-        new_node.prev, new_node.next = node, a
-        a.prev = new_node
+            result = result.next
+            if result.next:
+                headq.headpush(heap, (result.next.val, idx, result.next))
 
-    def _del(self, node: Node()):
-        prev, next = node.prev, node.next
-        node.next.prev = prev
-        node.prev.next = next
-
-    def insertFront(self, value: int) -> bool:
-        if self.cnt == self.maxlen:
-            return False
-        else:
-            self.cnt += 1
-            self._add(self.head,Node(value))
-            return True
-
-    def insertLast(self, value: int) -> bool:
-        if self.cnt == self.maxlen:
-            return False
-        else:
-            self.cnt += 1
-            self._add(self.tail.prev, Node(value))
-            return True
-
-    def deleteFront(self) -> bool:
-        if self.cnt == 0:
-            return False
-        else:
-            self.cnt -= 1
-            self._del(self.head.next)
-            return True
-
-    def deleteLast(self) -> bool:
-        if self.cnt == 0:
-            return False
-        else:
-            self.cnt -= 1
-            self._del(self.tail.prev)
-            return True
-
-    def getFront(self) -> int:
-        if self.isEmpty():
-            return -1
-        else:
-            return self.head.next.val
-
-    def getRear(self) -> int:
-        if self.isEmpty():
-            return -1
-        else:
-            return self.tail.prev.val
-
-    def isEmpty(self) -> bool:
-        if self.cnt == 0:
-            return True
-        else:
-            return False        
-
-    def isFull(self) -> bool:
-        if self.cnt == self.maxlen:
-            return True
-        else:
-            return False
+        return root.next
