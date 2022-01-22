344c7bbe1318a263cad3cb142c8b29d9c5ad34ac
Hyuntae H <okht12@gmail.com>
2022-01-22 14:39:54 +0000

    .

--- Python_Algorithm_Book/Practice_Code/test1.py
+++ Python_Algorithm_Book/Practice_Code/test1.py
@@ -12,27 +12,59 @@ class Node:
 class MyHashMap:
 
     def __init__(self):
+        #init size = 1000, if over 1000 get same key(using individual chaining)
         self.size = 1000
         self.table = collections.defaultdict(Node)
 
     def put(self, key: int, value: int) -> None:
         index = key % self.size
+        #No Node -> append then exit.
+        if self.table[index].value is None:
+            self.table[index] = Node(key, value)
+            return
         
+        #When Key exist, build up the duplicated values with linked-list.
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                p.value = value
+                return
+            if p.next is None:
+                break
+            p = p.next
+        p.next = Node(key, value)
+
 
     def get(self, key: int) -> int:
-        for x in self.l: #x = [1, a]
-            if x[0] == key:
-                return x[1]
+        index = key % self.size
+        if self.table[index].value is None:
+            return -1
+
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                return p.value
+            p = p.next
         return -1
         
 
     def remove(self, key: int) -> None:
-        for x in self.l: #x = [1, a]
-            if x[0] == key:
-                self.l.remove(x)
+        index = key % self.size
+        if self.table[index].value is None:
+            return
+
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                self.table[index] = Node() if p.next is None else p.next
                 return
-        return
-        
+
+        prev = p
+        while p:
+            if p.key == key:
+                prev.next = p.next
+                return
+            prev, p = p, p.next        
 
 
 # Your MyHashMap object will be instantiated and called as such:
