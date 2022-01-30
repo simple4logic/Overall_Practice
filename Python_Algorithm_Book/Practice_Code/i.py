722e063559b6255dd9826e278d814fcdbaf24db2
Hyuntae H <okht12@gmail.com>
2022-01-30 14:43:10 +0000

    .

--- Python_Algorithm_Book/Practice_Code/i.py
+++ Python_Algorithm_Book/Practice_Code/i.py
@@ -4,40 +4,35 @@ import collections
 import heapq
 
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        def expand(left: int, right : int) -> str:
-            temp = set()
-            
-            while left>=0 and right<len(s) and (s[left] not in temp) and (s[right] not in temp):
-                if (right != left) and (s[right] == s[left]):
-                    break
-                temp.add(s[left])
-                temp.add(s[right])
-                left -= 1
-                right +=1  
-            #print("before exit", temp)
-            #print("this is length", len(s[left + 1:right]))
-            return s[left + 1:right]
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freqs = collections.Counter(nums)
+        freqs_heap = []
+        #insert to heap as negative
 
-        result = ''
-        for i in range(len(s)):
-            #print("loop", i)
-            result = max(result, expand(i, i+1), expand(i, i), key=len) #i, i+1 -> 짝수 case // i,i -> 홀수 case
+        for f in freqs:
+            #f is the key, freqs[f] is the value(frequency)
+            #insert it SWAPPED
+            heapq.heapqpush(freqs_heap, (-freqs[f], f))
 
-        return len(result)
+        topk = list()
+
+        for _ in range(k):
+            #pop out k-times
+            #min-heap, so pop from the smallest value
+            topk.append(heapq.heappop(freqs_heap[1]))
 
 
 a = Solution()
-# s = "abcabcbb"
-s = 'a'
-ans = a.lengthOfLongestSubstring(s)
+s = [1,1,2,2,2,2,3,5,5,5]
+ans = a.topKFrequent(s,2)
 print(ans)
 
-
 '''
-sliding window로 풀어보기
-1칸으로 시작해서 양옆으로 2칸씩 확장
-2칸으로 시작해서 양옆으로 2칸씩 확장
-중복이 검출 될 시에 현재 maxlen에 저장하고 
-다음 loop(start point)로 넘어가기
+1111 222 33
+
+1: 4개 , 2 : 3개, 3: 2개
+1, 2, 3 (key)
+개수(value)
+
+상위 k번!!
 '''
