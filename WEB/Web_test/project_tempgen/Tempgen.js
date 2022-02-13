7e77121e5a1bd4361239ab955b5c964bdd7d1f08
Hyuntae H <okht12@gmail.com>
2022-02-13 04:09:55 +0000

    .

--- WEB/Web_test/project_tempgen/Tempgen.js
+++ WEB/Web_test/project_tempgen/Tempgen.js
@@ -8,10 +8,14 @@ function printrand() {
     return;
   }
 
-  temp = Math.random() * (max - min)
-
-  ans = Math.floor(temp * 10) / 10 + min;
+  var roomname = ["경비1-", "경비2-", "하역1-", "하역2-", "본 부-", "장비정비-", "정리1-", "정리2-"];
   
-  document.getElementById("result").innerText = ans;
+  for(var i = 1 ; i < 6 ; i++){
+    temp = Math.random() * (max - min);
+    ans = Math.floor(temp * 10) / 10 + min;
+    objectid = "경비1-".concat(i);
+    document.getElementById(objectid).innerText = ans;
   }
+
+}
     
