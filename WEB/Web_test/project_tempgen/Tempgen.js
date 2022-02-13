d69d41ec3a0a5d8da3d393ba5d35210c27e68026
Hyuntae H <okht12@gmail.com>
2022-02-13 02:35:36 +0000

    .

--- WEB/Web_test/project_tempgen/Tempgen.js
+++ WEB/Web_test/project_tempgen/Tempgen.js
@@ -1,3 +1,15 @@
-function rand(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function printrand() {
+  const min = Number(document.getElementById('mininput').value);
+  const max = Number(document.getElementById('maxinput').value);
+
+  //잘못 된 값 입력시 배제
+  if(max < min){
+    window.alert("다시 입력해주세요!");
+    return;
   }
+
+  ans = Math.floor(Math.random() * (max - min + 1)) + min;
+  
+  document.getElementById("result").innerText = ans;
+  }
+    
