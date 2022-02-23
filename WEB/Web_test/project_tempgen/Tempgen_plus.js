e8b8a4911093460f1904fa522ca5cc344fea4365
Hyuntae H <okht12@gmail.com>
2022-02-23 10:51:02 +0000

    .

--- WEB/Web_test/project_tempgen/Tempgen_plus.js
+++ WEB/Web_test/project_tempgen/Tempgen_plus.js
@@ -12,18 +12,30 @@ function printrand() {
     return;
   }
   
-  for(var j = 0; j < 8 ; j++){
-    for(var i = 1 ; i < 6 ; i++){
+  for(var i = 1 ; i < 6 ; i++){ //j = roomname, i = timeline
+    var rooms = [];
+
+    for(var j = 0; j < 8 ; j++){
       temp = Math.random() * (max - min);
       ans = Math.floor(temp * 10) / 10 + min;
+      rooms.push(ans);
       
-      objectid = ''.concat(roomname[j],i); //j = roomname, i = timeline
+      objectid = ''.concat(roomname[j],i);
       document.getElementById(objectid).innerText = ans;
     }
+
+    //결과적으로 rooms라는 배열 안에는 지금 특정 시간의 모든 생활관 온도가 기록되어 있음.
+    //[1, 8, 6, 7, 5, 3, 4, 2]
+    console.log(rooms);
+    min_idx = rooms.indexOf(Math.max.apply(null, rooms));
+    minid = ''.concat(roomname[min_idx],i);
+    console.log(minid);
+    document.getElementById("minid").style.color = "blue";
+    // maxid = ''.concat(roomname[rooms.indexOf(Math.max(rooms))],i);
+    // document.getElementById("maxid").style.color = "red";
+    
   }
 }
-
-
 
 
 /* 최대 입력할 수 있는 숫자의 길이를 제한하는 함수 */
