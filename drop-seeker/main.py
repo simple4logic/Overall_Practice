30a75aaa1d04091cfe946cb271a644412c247599
Hyuntae H <okht12@gmail.com>
2022-03-01 14:30:11 +0000

    .

--- drop-seeker/main.py
+++ drop-seeker/main.py
@@ -1,3 +1,5 @@
+from selenium import webdriver
+import time
 import requests
 from bs4 import BeautifulSoup as bs
 
@@ -7,23 +9,31 @@ LOGIN_INFO = {
     'userPassword': 'Hyuntae12#$'
 }
 
+url = "https://sugang.snu.ac.kr/"
+
+driver.get(url)
+time.sleep(1)
+
 # Session 생성, with 구문 안에서 유지
 with requests.Session() as s:
     # HTTP POST request: 로그인을 위해 POST url와 함께 전송될 data를 넣어주자.
     login_req = s.post('https://sugang.snu.ac.kr/', data=LOGIN_INFO)
-    # 어떤 결과가 나올까요?
+
     print(login_req.status_code)
 
     if login_req.status_code != 200:
         raise Exception('로그인 실패!')
 
-#x_path = '//*[@id="mbMenuCartEm"]'
+x_path1 = '//*[@id="header"]/div[2]/div[1]/button[1]'
+searchbox = driver.find_element_by_xpath(x_path1)
+searchbox.click()
+time.sleep(1)
 
-post_one = s.get('https://sugang.snu.ac.kr/')
-soup = bs(post_one.text, 'html.parser')
+x_path2 = '//*[@id="HD100"]/div/div/div[1]/fieldset/div[3]/button[1]'
+searchbox = driver.find_element_by_xpath(x_path2)
+searchbox.click()
+time.sleep(1)
 
-title = soup.select('#sugangGuideContents > div.con-box > div > span:nth-child(1) > font > b')
-print(title[0].text)
-
-##계속 list index out of range 오류가 뜬다.....
-
+html = driver.page_source
+soup = bs(html, 'html.parser')
+string = soup.select_one('#skip-con > div > div.course-list-wrap.pd-r > div.course-info-list > div:nth-child(1) > div.left > label > input[type=radio]:nth-child(1)')
