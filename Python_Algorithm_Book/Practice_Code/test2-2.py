41fd1256c533cb30456311f9ca95982a354b01ca
Hyuntae H <okht12@gmail.com>
2022-04-11 14:26:30 +0000

    .

--- Python_Algorithm_Book/Practice_Code/test2-2.py
+++ Python_Algorithm_Book/Practice_Code/test2-2.py
@@ -7,36 +7,8 @@ import re
 import collections
 import heapq
 
-class Solution:
-    def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        freq = {}
-        cnt = 0
-
-        #substitute counter module
-        for a in list(stones):
-            if a not in freq:
-                freq[a] = 1 #append new one
-            else:
-                freq[a] += 1
-
-        for b in list(jewels):
-            num = freq[b]
-            if num != 0:
-                cnt += num
-
-        return cnt
-
-
-# word = input()
-# print(word)
-# counter = collections.Counter(word)
-# print(counter)
-# print(counter['x'])
-
-a = Solution()
-J = "aA"
-S = "aAAbbbb"
-A = a.numJewelsInStones(J, S)
-print(A)
+date_info = {'year':'2020', 'month':'01', 'day':'7'}
+new_info = {**date_info}
+print(new_info)
 
 
