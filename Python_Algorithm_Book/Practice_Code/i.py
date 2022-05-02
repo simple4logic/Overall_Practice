d0eaff2355823106db39679ebf57f3724b84809c
Hyuntae H <okht12@gmail.com>
2022-05-02 14:49:21 +0000

    .

--- Python_Algorithm_Book/Practice_Code/i.py
+++ Python_Algorithm_Book/Practice_Code/i.py
@@ -1,27 +1,21 @@
 from typing import List
 import re
 import collections
-import heapq
 
+# 첫 1에서 시작하는건 잘 모르겠고 중간부터 일단 구현해봄
 
-graph={
-    1:[2,3,4],
-    2:[5],
-    3:[5],
-    4:[],
-    5:[6,7],
-    6:[],
-    7:[3]
-}
+def dfs(start_point :(int, int)):
+    stack = [start_point]
+    discovered = []
+    near = []
 
-def recursive_dfs(v, discovered=[]):
-    discovered.append(v)
-    for w in graph[v]:
-        if w not in discovered:
-            discovered = recursive_dfs(w, discovered)
-    return discovered
+    while stack:
+        v = stack.pop()
+        if v not in discovered:
+            discovered.append(v)
+            #여기서 v 상, 하, 좌, 우 순으로 탐색해서 1인 좌표만 찾아서 임시 리스트 near에 저장해두셈
+            for tuples in near:
+                stack.append(tuples)
 
-
-ans = recursive_dfs(1)
-
-print(ans)
+    # 그다음 뭘 리턴해야할지는 잘 몰루? 아마 한번 섬 모두다 탐색 완료했으면 전역변수에 +1 리턴하고
+    # 다음 탐색 들어가기 ㄱㄱ 다음에는 not discovered인 동시에 1인 point를 인수로 넣어서 확인하면 되겠네
--- README_B4_BEGIN.md
+++ README_B4_BEGIN.md
@@ -39,4 +39,4 @@ https://velog.io/@chappi/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-5%EC%9D%BC%EC%B0%A
 >https://dojang.io/mod/page/view.php?id=2368
 
 ## 10.
-마지막으로 dfs 와 bfs 알고리즘 정리하고 있었음
+q32 문제 대충 알고리즘 짜고 있었음 practice code 중 i.py 참고
