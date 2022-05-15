9cfaff9f123c2c6c2a466f0a89a7b9256d066eb1
Hyuntae H <okht12@gmail.com>
2022-05-15 14:43:49 +0000

    .

--- Python_Algorithm_Book/Practice_Code/test1.py
+++ Python_Algorithm_Book/Practice_Code/test1.py
@@ -1,84 +1,28 @@
 from typing import List
-import re
-import collections
-import heapq
 
-class Node:
-    def __init__(self, key = None, value = None):
-        self.key = key
-        self.value = value
-        self.next = None
-
-class MyHashMap:
-
-    def __init__(self):
-        #init size = 1000, if over 1000 get same key(using individual chaining)
-        self.size = 1000
-        self.table = collections.defaultdict(Node)
-
-    def put(self, key: int, value: int) -> None:
-        index = key % self.size
-        #No Node -> append then exit.
-        if self.table[index].value is None:
-            self.table[index] = Node(key, value)
-            return
-        
-        #When Key exist, build up the duplicated values with linked-list.
-        p = self.table[index]
-        while p:
-            if p.key == key:
-                p.value = value
-                return
-            if p.next is None:
-                break
-            p = p.next
-        p.next = Node(key, value)
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        sero = len(grid)
+        garo = len(grid[0])
+        discovered = [] #탐색 완료된 그리드 상의 좌표
+        nums = 0
+    
+        for i in range(sero): #세로 길이
+            for j in range(garo): #가로 길이
+                if grid[i][j] == "0" or (i,j) in discovered:
+                    continue
+                else: 
+                    iteration(i,j)
+                    nums += 1
+        return nums
 
 
-    def get(self, key: int) -> int:
-        index = key % self.size
-        if self.table[index].value is None:
-            return -1
-
-        p = self.table[index]
-        while p:
-            if p.key == key:
-                return p.value
-            p = p.next
-        return -1
-        
-
-    def remove(self, key: int) -> None:
-        index = key % self.size
-        if self.table[index].value is None:
-            return
-
-        p = self.table[index]
-        while p:
-            if p.key == key:
-                self.table[index] = Node() if p.next is None else p.next
-                return
-
-        prev = p
-        while p:
-            if p.key == key:
-                prev.next = p.next
-                return
-            prev, p = p, p.next
-
-
-# Your MyHashMap object will be instantiated and called as such:
-a = MyHashMap()
-#obj.put(key,value)
-a.put(1, 'a')
-a.put(2, 'f')
-a.put(3, 'd')
-a.put(6, 'g')
-print(a.l)
-print(a.get(3))
-a.remove(3)
-print(a.get(3))
-print(a.l)
-a.put(1, 'x')
-print(a.l)
-#obj.remove(key)
+def recursive(i: int, j: int):
+    discovered.append((i,j))
+        for tuples in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+            if tuples[0] >= sero or tuples[0] < 0 or tuples[1] >= garo or tuples[1] < 0:
+                continue
+            if grid[tuples[0]][tuples[1]] == "0":
+                continue
+            self.recursive(tuples)
+    return
