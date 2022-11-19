35dab787999c41bf37ed863b7663021b86b872b3
Hyuntae H <68508521+simple4logic@users.noreply.github.com>
2022-11-20 00:43:26 +0900

    .

--- Python_Algorithm_Book/Practice_Code/i.py
+++ Python_Algorithm_Book/Practice_Code/i.py
@@ -4,13 +4,27 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         numdic = {'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
         ans: List = []
-        size = len(digits)
+        fullsize = len(digits)
 
-        def dfs(size):
-            tmp: str = ''
+        def dfs(size: int, tmp: str):
+            if size <= 1:
+                ans.append(tmp)
+                return
+
+            for B in numdic[digits[fullsize - size + 1]]:
+                dfs(size-1, tmp+B)
             
+
+        
+        if(digits):
+            for A in numdic[digits[0]]:
+                tmp: str = ''
+                dfs(fullsize, tmp+A)
+
+        return ans
+
 
 
 a = Solution()
 
-a.letterCombinations("23")
+a.letterCombinations("222")
